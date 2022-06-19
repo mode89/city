8084a7f4e384e5618c69f428b9d6432e4d5aa446
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 18:32:38 +0800

    Terminate window on exception
    Allow restarting the UI loop.

--- src/city/core.clj
+++ src/city/core.clj
@@ -49,15 +49,17 @@
 (defn ui [window-promise]
   (GLFW/glfwInit)
   (let [window (GLFW/glfwCreateWindow 800 600 "City" 0 0)]
-    (deliver window-promise window)
-    (GLFW/glfwMakeContextCurrent window)
-    (GLFW/glfwSwapInterval 1)
-    (GL/createCapabilities)
-    (GL45/glClearColor 0.1 0.1 0.5 1.0)
-    (while (not (GLFW/glfwWindowShouldClose window))
-      (tick window))
-    (GLFW/glfwDestroyWindow window)
-    (GLFW/glfwTerminate)))
+    (try
+      (deliver window-promise window)
+      (GLFW/glfwMakeContextCurrent window)
+      (GLFW/glfwSwapInterval 1)
+      (GL/createCapabilities)
+      (GL45/glClearColor 0.1 0.1 0.5 1.0)
+      (while (not (GLFW/glfwWindowShouldClose window))
+        (tick window))
+      (finally
+        (GLFW/glfwDestroyWindow window)
+        (GLFW/glfwTerminate)))))
 
 (defn start-ui-thread []
   (let [window (promise)]
