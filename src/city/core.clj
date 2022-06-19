742ca706890563ab085a26669805d5bda5096694
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 16:30:37 +0800

    Move code into tick function
    Allows overriding the main loop inside REPL.

--- src/city/core.clj
+++ src/city/core.clj
@@ -37,6 +37,15 @@
 (defn send-command [cmd]
   (reset! command cmd))
 
+(defn tick [window]
+  (when (some? @command)
+    (@command)
+    (reset! command nil))
+  (GLFW/glfwPollEvents)
+  (GL45/glClear (bit-or GL45/GL_COLOR_BUFFER_BIT
+                        GL45/GL_DEPTH_BUFFER_BIT))
+  (GLFW/glfwSwapBuffers window))
+
 (defn ui [window-promise]
   (GLFW/glfwInit)
   (let [window (GLFW/glfwCreateWindow 800 600 "City" 0 0)]
@@ -46,13 +55,7 @@
     (GL/createCapabilities)
     (GL45/glClearColor 0.1 0.1 0.5 1.0)
     (while (not (GLFW/glfwWindowShouldClose window))
-      (when (some? @command)
-        (@command)
-        (reset! command nil))
-      (GLFW/glfwPollEvents)
-      (GL45/glClear (bit-or GL45/GL_COLOR_BUFFER_BIT
-                            GL45/GL_DEPTH_BUFFER_BIT))
-      (GLFW/glfwSwapBuffers window))
+      (tick window))
     (GLFW/glfwDestroyWindow window)
     (GLFW/glfwTerminate)))
 
