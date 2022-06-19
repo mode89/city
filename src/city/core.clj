5cc3445d6f62bc4f55b16d750dc4dab79c97c7e4
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 13:58:35 +0800

    Routine for compiling a shader

--- src/city/core.clj
+++ src/city/core.clj
@@ -2,6 +2,21 @@
   (:import (org.lwjgl.glfw GLFW)
            (org.lwjgl.opengl GL GL45)))
 
+(defn compiler-shader [shader-type source]
+  (let [shader (GL45/glCreateShader
+                 ({:vertex GL45/GL_VERTEX_SHADER
+                   :fragment GL45/GL_FRAGMENT_SHADER} shader-type))]
+    (GL45/glShaderSource shader source)
+    (GL45/glCompileShader shader)
+    (if (= (GL45/glGetShaderi shader GL45/GL_COMPILE_STATUS) GL45/GL_TRUE)
+      shader
+      (let [info-log-length (GL45/glGetShaderi shader
+                                               GL45/GL_INFO_LOG_LENGTH)]
+        (println "Failed to compile shader:")
+        (println (GL45/glGetShaderInfoLog shader info-log-length))
+        (GL45/glDeleteShader shader)
+        nil))))
+
 (defn ui [window-promise]
   (GLFW/glfwInit)
   (let [window (GLFW/glfwCreateWindow 800 600 "City" 0 0)]
