a35e3f7c6d1cfee3cffb31705d5d2c5f71ebba3e
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 10:35:48 +0800

    Hello-world project
    Setting up the base of the leiningen project.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject city "0.1.0"
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+  ]
+  :main city.core)
---
+++ src/city/core.clj
@@ -0,0 +1,4 @@
+(ns city.core)
+
+(defn -main [& args]
+  (println "Hello, World!"))
