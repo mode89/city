999f269fb32c4191443470d6f89024394bd913aa
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 11:40:37 +0800

    Close window and terminate GLFW
    Cleaning up resources.

--- src/city/core.clj
+++ src/city/core.clj
@@ -9,7 +9,10 @@
     (GLFW/glfwSwapInterval 1)
     (GL/createCapabilities)
     (GL45/glClearColor 0.1 0.1 0.5 1.0)
-    (while true
+    (while (not (GLFW/glfwWindowShouldClose window))
+      (GLFW/glfwPollEvents)
       (GL45/glClear (bit-or GL45/GL_COLOR_BUFFER_BIT
                             GL45/GL_DEPTH_BUFFER_BIT))
-      (GLFW/glfwSwapBuffers window))))
+      (GLFW/glfwSwapBuffers window))
+    (GLFW/glfwDestroyWindow window)
+    (GLFW/glfwTerminate)))
