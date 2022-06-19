5c084d7a2f0df033cb1a3dc514a2d535a1f17d41
Andrey Krainyak <mode.andrew@gmail.com>
2022-06-19 11:34:04 +0800

    Create window and clear it in the main loop
    The window is going to be used for displaying graphics.

--- src/city/core.clj
+++ src/city/core.clj
@@ -1,4 +1,15 @@
-(ns city.core)
+(ns city.core
+  (:import (org.lwjgl.glfw GLFW)
+           (org.lwjgl.opengl GL GL45)))
 
 (defn -main [& args]
-  (println "Hello, World!"))
+  (GLFW/glfwInit)
+  (let [window (GLFW/glfwCreateWindow 800 600 "City" 0 0)]
+    (GLFW/glfwMakeContextCurrent window)
+    (GLFW/glfwSwapInterval 1)
+    (GL/createCapabilities)
+    (GL45/glClearColor 0.1 0.1 0.5 1.0)
+    (while true
+      (GL45/glClear (bit-or GL45/GL_COLOR_BUFFER_BIT
+                            GL45/GL_DEPTH_BUFFER_BIT))
+      (GLFW/glfwSwapBuffers window))))
